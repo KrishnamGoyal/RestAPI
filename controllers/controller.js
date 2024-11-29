7d8e38c44ca9b5d2f6bd18f3a86105ae469db9bc
KrishnamGoyal <krishnam61000@gmail.com>
2024-11-30 01:59:08 +0530

    Implemented req.query() in api

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,31 +1,13 @@
 const User = require("../models/user");
 
 const getAllUsers = async (req, res) => {
-  const users = await usersDb();
+  const users = await User.find(req.query);
   res.status(200).json(users);
 };
 
 const getAllUsersTesting = async (req, res) => {
-  const users = await usersDbTesting();
+  const users = await User.find(req.query);
   res.status(200).json(users);
-};
-
-const usersDb = async (arg) => {
-  try {
-    const users = await User.find();
-    return users;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const usersDbTesting = async (arg) => {
-  try {
-    const users = await User.find({ type: "testing" });
-    return users;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 module.exports = { getAllUsers, getAllUsersTesting };
